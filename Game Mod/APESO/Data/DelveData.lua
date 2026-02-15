59bdd48c3b997d2c9d55fbd4af0c05fa733a7e0d
spencer2585 <spencer2585@gmail.com>
2026-02-14 23:03:45 -0500

    Start adding Delve table

---
+++ Game Mod/APESO/Data/DelveData.lua
@@ -0,0 +1,4 @@
+APESO_DelveData = {
+    -- ZoneId as parent, saved killed bosses in savedVariables then check if all bosses in the zone are killed if yes save dungeon as cleared
+        [270] = {name = "Shrine of the black maw", locationId = 73, bosses = {"Peers-Through-Glass"}},
+    }
